ef550781b942c18ee8cb04295394d278d1e835d4
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-03 21:58:15 +0200

    Add register && security-bundle

---
+++ app/src/Controller/RegisterController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+
+use App\Entity\User;
+use App\Form\RegistrationForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+
+
+final class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'register_index')]
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashedPassword = $hasher->hashPassword($user, $plainPassword);
+
+            $user->setPassword($hashedPassword);
+            $user->setIsAdmin(false);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('login_index');
+        }
+
+//        return $this->render('register/index.html.twig', [
+//            'controller_name' => 'RegisterController',
+//        ]);
+        return $this->render('register/index.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+
+        // $post = new Post();
+
+
         // $product = new Product();
         // $manager->persist($product);
 
