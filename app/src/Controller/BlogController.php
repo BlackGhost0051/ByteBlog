a621d6aec8be0d1928be1cd926f56b0bf8951d5e
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-04 14:53:40 +0200

    Add post form

--- app/src/Controller/BlogController.php
+++ app/src/Controller/BlogController.php
@@ -6,21 +6,56 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Repository\PostRepository;
+use App\Entity\Post;
+use App\Form\AddPostForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
 final class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'blog_index')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+
+        $posts = $postRepository->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'posts' => $posts,
         ]);
     }
 
-    #[Route('/add_post', name: 'add_post_index')]
-    public function add_post(): Response
+    #[Route('/blog/add_post', name: 'add_post_index')]
+    public function add_post(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        $post = new Post();
+
+        $form = $this->createForm(AddPostForm::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setCreatedAt(new \DateTimeImmutable());
+
+            $em->persist($post);
+            $em->flush();
+
+            $this->addFlash('success', 'Post added successfully!');
+
+            return $this->redirectToRoute('blog_index');
+        }
+
+        return $this->render('blog/add_post.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route('/blog/post/{id}', name: 'blog_post')]
+    public function show(Post $post): Response
+    {
+        return $this->render('blog/post.html.twig', [
+            'post' => $post,
         ]);
     }
 }
