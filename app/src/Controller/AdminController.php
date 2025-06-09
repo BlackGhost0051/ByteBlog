03276b902190d184fbfa491ddf5a64127fdc3f2b
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-09 18:20:29 +0200

    Add admin delete functional

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -7,6 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\UserRepository;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
+
 final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin_index')]
@@ -17,5 +24,33 @@ final class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'users' => $users,
         ]);
+    }
+
+
+    #[Route('/admin/delete/{id}', name: 'admin_user_delete', methods: ['POST'])]
+    public function delete(
+        int $id,
+        UserRepository $userRepository,
+        EntityManagerInterface $em,
+        Request $request,
+        CsrfTokenManagerInterface $csrfTokenManager
+    ): RedirectResponse {
+        $token = $request->request->get('_token');
+        if (!$csrfTokenManager->isTokenValid(new CsrfToken('delete_user_' . $id, $token))) {
+            $this->addFlash('error', 'Invalid CSRF token.');
+            return $this->redirectToRoute('admin_index');
+        }
+
+        $user = $userRepository->find($id);
+
+        if (!$user) {
+            $this->addFlash('error', 'User not found.');
+        } else {
+            $em->remove($user);
+            $em->flush();
+            $this->addFlash('success', 'User deleted successfully.');
+        }
+
+        return $this->redirectToRoute('admin_index');
     }
 }
